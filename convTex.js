81bae1664620b28e530f7aed7124164962c9b61b
Job <jobtalle@hotmail.com>
2018-12-28 14:47:51 +0100

    Added a displacement shader example

--- convTex.js
+++ convTex.js
@@ -15,12 +15,31 @@ const ConvTex = function(myr, shader, width, height) {
         _surfaces[1 - _front].clear();
 
         shader.setSurface("source", _surfaces[_front]);
+        myr.blendDisable();
         shader.draw(0, 0);
+        myr.blendEnable();
+
+        flip();
+    };
+
+    this.apply = applicationShader => {
+        _surfaces[1 - _front].bind();
+        _surfaces[1 - _front].clear();
+
+        applicationShader.setSurface("target", _surfaces[_front]);
+        myr.blendDisable();
+        applicationShader.draw(0, 0);
+        myr.blendEnable();
 
         flip();
     };
 
     this.getFront = () => {
         return _surfaces[_front];
+    };
+
+    this.setClearColor = color => {
+        for (const surface of _surfaces)
+            surface.setClearColor(color);
     };
 };
